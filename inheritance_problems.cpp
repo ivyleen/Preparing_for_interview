bbebda9c9f37d199da46116529b31c4b4e63560c
Ivy Ivanova <ivaivanova091@gmail.com>
2020-07-30 16:34:46 +0300

    [inheritance] why we need virtual polymorphism

--- inheritance_problems.cpp
+++ inheritance_problems.cpp
@@ -8,9 +8,9 @@ class Base
 {
 public:
         Base () { cout << "Base default constructor called." << endl; }
-        ~Base () { cout << "Base destructor called." << endl; }
+        virtual ~Base () { cout << "Base destructor called." << endl; }
 
-        void func () { cout << "Base function called." << endl; }
+        virtual void func () { cout << "Base function called." << endl; }
 };
 
 
@@ -30,12 +30,18 @@ int main ()
 	Base * b_ptr = new Base();
 	Base * d_ptr = new Derived();
 
+	// calling a function
+	b_ptr->func(); // works fine
+	d_ptr->func(); // in order to call the function of the Derived class we need to have
+		       // properly working run time virtual polymorphism 
+		       // or in other words we need to mark the Base function as virtual
+
 	// destruction
 	delete b_ptr; // works fine
 	b_ptr = nullptr;
 
-	delete d_ptr; // doesn't actually calles for the Derived destructor
-       		      // that's why we need virtual keyword infront of the destructor
+	delete d_ptr; // doesn't actually calls for the Derived destructor
+       		      // that's why we need virtual keyword in front of the base destructor
 	d_ptr = nullptr;
 	        
         return 0;
